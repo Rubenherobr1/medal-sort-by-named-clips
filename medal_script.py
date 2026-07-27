3a0b1344b7f8fb8c49e5925131740f9f65f98e72
Rubenherobr1 <196861340+Rubenherobr1@users.noreply.github.com>
2026-07-27 18:08:28 +0100

    style: generalize decodeTitle to decodeStrType

--- medal_script.py
+++ medal_script.py
@@ -88,39 +88,39 @@ def normBin(decimalByte):
     return byte
 
 
-def decodeTitle(metadata, titleIDPos):
-    titleID = normBin(metadata[titleIDPos])
-    sizeID = int(titleID[:4], 2)
+def decodeStrType(metadata, strIDPos):
+    strID = normBin(metadata[strIDPos])
+    sizeID = int(strID[:4], 2)
     
     str8, str16 = int("C", 16), int("D", 16) 
 
 
-    # the length of the title is in the next byte
+    # the length of the str is in the next byte
     if sizeID == str8:
-        titleLen = metadata[titleIDPos + 1]
-        titlePos = titleIDPos + 2
+        strLen = metadata[strIDPos + 1]
+        strPos = strIDPos + 2
 
-    # the length of the title is the next 2 bytes, as if the 1st of the 
-    # two bytes was read together with the second byte. This is the limit
-    # for medal clip names (280 bytes)
+    # the length of the str is the next 2 bytes, as if the 1st of the 
+    # two bytes was read together with the second byte
 
     elif sizeID == str16:
         byte1, byte2 = (
-            normBin(metadata[titleIDPos + i]) for i in (1, 2)
+            normBin(metadata[strIDPos + i]) for i in (1, 2)
         )
 
-        titleLen = int(byte1 + byte2, 2)
-        titlePos = titleIDPos + 3
+        strLen = int(byte1 + byte2, 2)
+        strPos = strIDPos + 3
 
-    # the length of the title is the 1st nibble of the titleID byte
+    # the length of the str is the 1st nibble of the strID byte
     else:
         if sizeID == 0:
             return None
         
-        titleLen = sizeID
-        titlePos = titleIDPos + 1
+        strLen = sizeID
+        strPos = strIDPos + 1
 
-    return metadata[titlePos : titlePos + titleLen].decode("utf-8")
+    return metadata[strPos : strPos + strLen].decode("utf-8")
+
 
 
 # find db path
@@ -166,7 +166,7 @@ for id, path, metadata in resultSet:
 
     # get the title, check if it exists
     titleIDPos = findTitlePos(metadata)
-    title = decodeTitle(metadata, titleIDPos)
+    title = decodeStrType(metadata, titleIDPos)
 
     if title is None: 
         continue
